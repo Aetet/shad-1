c311ba88bab89be2a2f3d64424a4aad95097df1d
Vyacheslav Kondratyuk <v.kondratyuk@ism-ukraine.com>
2013-05-29 11:18:18 +0300

    C string with null byte concat.

--- strings/concat/run.cpp
+++ strings/concat/run.cpp
@@ -24,14 +24,23 @@ void c_strcat() {
 void c_concat() {
 	char hello[] = { 'H', 'e', 'l', 'l', 'o', '\0' };
 	char world[] = { ',', ' ', 'W', 'o', 'r', 'l', 'd', '!', '\0' };
+	
+	for (int i = 0; i < 6; i++) {
+		if (hello[i] == '\0') {
+        	for (int j = 0; j < 9; j++) {
+                int index = 5 + j;
+                hello[index] = world[j];
+    		} 
+	    }
+	}
 
-	cout << hello << world << endl;
+	cout << hello << endl;
 }
 
 int main() {
 	cpp_concat();
 	c_strcat();
-	c_concat();
+	//c_concat();
 
 	return 0;
 }
