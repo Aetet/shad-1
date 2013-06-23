347ca4784a236371f6415a536b2065f2550e57ff
Vyacheslav Kondratyuk <viakondratiuk@gmail.com>
2013-06-23 14:23:41 +0300

    Matrix fibonacci

--- tasks/fibonacci/run.cpp
+++ tasks/fibonacci/run.cpp
@@ -6,7 +6,6 @@ int calls_count = 0;
 
 int fib_recursion(int n)
 {
-	cout << "Call: " << ++calls_count << endl;
 	if (n <= 2) { 
 		return 1;
 	} else {
@@ -29,11 +28,43 @@ int fib_dynamic(int n)
 	return fib;
 }
 
+int fib_matrix(int n)
+{
+  int a = 1, ta, 
+      b = 1, tb,
+      c = 1, rc = 0,  tc,
+      d = 0, rd = 1; 
+ 
+  while (n) { 
+    if (n & 1) {
+      // Умножаем вектор R на матрицу A
+      tc = rc;
+      rc = rc*a + rd*c;
+      rd = tc*b + rd*d;
+    } 
+ 
+    // Умножаем матрицу A на саму себя
+    ta = a; tb = b; tc = c;
+    a = a*a  + b*c;
+    b = ta*b + b*d;
+    c = c*ta + d*c;     
+    d = tc*tb+ d*d;
+ 
+    n >>= 1;  // Уменьшаем степень вдвое
+ 
+  }
+
+  return rc;
+}
+
 int main()
 {
 	int n = 4;
 	cout << "Recusrsion fib " << n << ": " << fib_recursion(n) << endl;
 
 	int m = 7;
-	cout << "Recusrsion fib " << m << ": " << fib_dynamic(m) << endl;	 
+	cout << "Dynamic fib " << m << ": " << fib_dynamic(m) << endl;
+
+	int d = 8;
+	cout << "Matrix fib" << d << " : " << fib_matrix(d) << endl;
 }
